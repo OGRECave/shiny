bf0c83d4257de3bfee70791d27d0308c9c19282f
scrawl <scrawl@baseoftrash.de>
2012-07-06 10:17:56 +0200

    direct_texture

--- Platforms/Ogre/OgreTextureUnitState.cpp
+++ Platforms/Ogre/OgreTextureUnitState.cpp
@@ -22,6 +22,10 @@ namespace sh
 			// delegate up
 			return TextureUnitState::setPropertyOverride (name, value, context);
 		}
+		else if (name == "direct_texture")
+		{
+			setTextureName (retrieveValue<StringValue>(value, context).get());
+		}
 		else if (name == "create_in_ffp")
 			return true; // handled elsewhere
 
