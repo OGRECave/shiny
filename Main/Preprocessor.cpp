33adca3b7e27d0bc32e8fb0ce2a0243379af1fe0
scrawl <scrawl@baseoftrash.de>
2012-07-22 00:49:17 +0200

    switch to boost::wave again, with threading disabled it's not that slow

--- Main/Preprocessor.cpp
+++ Main/Preprocessor.cpp
@@ -1,60 +1,100 @@
 #include "Preprocessor.hpp"
 
-#include "../mcpp/mcpp_lib.h"
-
-#include <cstdio>
-#include <iostream>
-
-#include <fstream>
+#include <boost/wave.hpp>
+#include <boost/wave/cpplexer/cpp_lex_token.hpp>
+#include <boost/wave/cpplexer/cpp_lex_iterator.hpp>
 
 namespace sh
 {
 	std::string Preprocessor::preprocess (std::string source, const std::string& includePath, std::vector<std::string> definitions, const std::string& name)
 	{
-		FILE* temp_file;
-		temp_file = tmpfile();
+		std::stringstream returnString;
 
-		fwrite(source.c_str(), sizeof(char), source.size(), temp_file);
-		rewind(temp_file);
+		// current file position is saved for exception handling
+		boost::wave::util::file_position_type current_position;
 
-		mcpp_use_mem_buffers(1);
-
-		int num_args = 3 + definitions.size()*2;
-		char* args[num_args];
-		char mcpp_str[] = "mcpp";
-		char _i[] = "-I";
-		char _d[] = "-D";
-		args[0] = mcpp_str;
-		args[1] = _i;
-
-		std::vector<char> writable(includePath.size()+1);
-		std::copy(includePath.begin(), includePath.end(), writable.begin());
-		char* include = &writable[0];
-
-		args[2] = include;
-
-		std::vector<char> vectors[definitions.size()];
-
-		int i=3;
-		int cur=0;
-		for (std::vector<std::string>::iterator it = definitions.begin(); it != definitions.end(); ++it)
+		try
 		{
-			args[i] = _d;
-			++i;
+			//  This token type is one of the central types used throughout the library.
+			//  It is a template parameter to some of the public classes and instances
+			//  of this type are returned from the iterators.
+			typedef boost::wave::cpplexer::lex_token<> token_type;
 
-			std::string val = *it;
-			std::vector<char> writable2(val.size()+1);
-			std::copy(val.begin(), val.end(), writable2.begin());
-			vectors[cur] = writable2;
+			//  The template boost::wave::cpplexer::lex_iterator<> is the lexer type to
+			//  to use as the token source for the preprocessing engine. It is
+			//  parametrized with the token type.
+			typedef boost::wave::cpplexer::lex_iterator<token_type> lex_iterator_type;
 
-			args[i] = &vectors[cur][0];
-			++cur;
-			++i;
+			//  This is the resulting context type. The first template parameter should
+			//  match the iterator type used during construction of the context
+			//  instance (see below). It is the type of the underlying input stream.
+			typedef boost::wave::context<std::string::iterator, lex_iterator_type
+					, boost::wave::iteration_context_policies::load_file_to_string,
+					emit_custom_line_directives_hooks>
+				context_type;
+
+			//  The preprocessor iterator shouldn't be constructed directly. It is
+			//  generated through a wave::context<> object. This wave:context<> object
+			//  is additionally used to initialize and define different parameters of
+			//  the actual preprocessing.
+			//
+			//  The preprocessing of the input stream is done on the fly behind the
+			//  scenes during iteration over the range of context_type::iterator_type
+			//  instances.
+			context_type ctx (source.begin(), source.end(), name.c_str());
+			ctx.add_include_path(includePath.c_str());
+			for (std::vector<std::string>::iterator it = definitions.begin(); it != definitions.end(); ++it)
+			{
+				ctx.add_macro_definition(*it);
+			}
+
+			//  Get the preprocessor iterators and use them to generate the token
+			//  sequence.
+			context_type::iterator_type first = ctx.begin();
+			context_type::iterator_type last = ctx.end();
+
+			//  The input stream is preprocessed for you while iterating over the range
+			//  [first, last). The dereferenced iterator returns tokens holding
+			//  information about the preprocessed input stream, such as token type,
+			//  token value, and position.
+			while (first != last)
+			{
+				current_position = (*first).get_position();
+				returnString << (*first).get_value();
+				++first;
+			}
+		}
+		catch (boost::wave::cpp_exception const& e)
+		{
+			// some preprocessing error
+			std::stringstream error;
+			error
+				<< e.file_name() << "(" << e.line_no() << "): "
+				<< e.description();
+			throw std::runtime_error(error.str());
+		}
+		catch (std::exception const& e)
+		{
+			// use last recognized token to retrieve the error position
+			std::stringstream error;
+			error
+				<< current_position.get_file()
+				<< "(" << current_position.get_line() << "): "
+				<< "exception caught: " << e.what();
+			throw std::runtime_error(error.str());
+		}
+		catch (...)
+		{
+			// use last recognized token to retrieve the error position
+			std::stringstream error;
+			error
+				<< current_position.get_file()
+				<< "(" << current_position.get_line() << "): "
+				<< "unexpected exception caught.";
+			throw std::runtime_error(error.str());
 		}
 
-		mcpp_lib_main(num_args, args, temp_file);
-
-		char* result = mcpp_get_mem_buffer (OUT);
-		return std::string(result);
+		std::cout << returnString.str() << std::endl;
+		return returnString.str();
 	}
 }
--- Main/Preprocessor.hpp
+++ Main/Preprocessor.hpp
@@ -4,6 +4,20 @@
 #include <string>
 #include <vector>
 
+#include <cstdio>
+#include <ostream>
+#include <string>
+#include <algorithm>
+
+#include <boost/assert.hpp>
+#include <boost/config.hpp>
+
+#include <boost/wave/cpp_throw.hpp>
+#include <boost/wave/cpp_exceptions.hpp>
+#include <boost/wave/token_ids.hpp>
+#include <boost/wave/util/macro_helpers.hpp>
+#include <boost/wave/preprocessing_hooks.hpp>
+
 namespace sh
 {
 	/**
@@ -22,6 +36,90 @@ namespace sh
 		 */
 		static std::string preprocess (std::string source, const std::string& includePath, std::vector<std::string> definitions, const std::string& name);
 	};
+
+
+	class emit_custom_line_directives_hooks
+	:   public boost::wave::context_policies::default_preprocessing_hooks
+	{
+	public:
+		///////////////////////////////////////////////////////////////////////////
+		//
+		//  The function 'emit_line_directive' is called whenever a #line directive
+		//  has to be emitted into the generated output.
+		//
+		//  The parameter 'ctx' is a reference to the context object used for
+		//  instantiating the preprocessing iterators by the user.
+		//
+		//  The parameter 'pending' may be used to push tokens back into the input
+		//  stream, which are to be used instead of the default output generated
+		//  for the #line directive.
+		//
+		//  The parameter 'act_token' contains the actual #pragma token, which may
+		//  be used for error output. The line number stored in this token can be
+		//  used as the line number emitted as part of the #line directive.
+		//
+		//  If the return value is 'false', a default #line directive is emitted
+		//  by the library. A return value of 'true' will inhibit any further
+		//  actions, the tokens contained in 'pending' will be copied verbatim
+		//  to the output.
+		//
+		///////////////////////////////////////////////////////////////////////////
+		template <typename ContextT, typename ContainerT>
+		bool
+		emit_line_directive(ContextT const& ctx, ContainerT &pending,
+			typename ContextT::token_type const& act_token)
+		{
+		// emit a #line directive showing the relative filename instead
+		typename ContextT::position_type pos = act_token.get_position();
+		unsigned int column = 1;
+
+			typedef typename ContextT::token_type result_type;
+			using namespace boost::wave;
+
+			pos.set_column(column);
+			pending.push_back(result_type(T_POUND, "#", pos));
+
+			pos.set_column(++column);      // account for '#'
+
+
+			pending.push_back(result_type(T_STRINGLIT, "line ", pos));
+			pos.set_column(column+5);                 // account for 'line '
+
+
+		// 21 is the max required size for a 64 bit integer represented as a
+		// string
+		char buffer[22];
+
+			sprintf (buffer, "%d", pos.get_line());
+
+			pending.push_back(result_type(T_INTLIT, buffer, pos));
+			pos.set_column(column += (unsigned int)strlen(buffer)); // account for <number>
+
+			pending.push_back(result_type(T_GENERATEDNEWLINE, "\n", pos));
+
+
+			/*
+
+			pending.push_back(result_type(T_SPACE, " ", pos));
+			pos.set_column(++column);                 // account for ' '
+
+		std::string file("\"");
+		boost::filesystem::path filename(
+			boost::wave::util::create_path(ctx.get_current_relative_filename().c_str()));
+
+			using boost::wave::util::impl::escape_lit;
+			file += escape_lit(boost::wave::util::native_file_string(filename)) + "\"";
+
+			pending.push_back(result_type(T_STRINGLIT, file.c_str(), pos));
+			pos.set_column(column += (unsigned int)file.size());    // account for filename
+
+			*/
+
+			return true;
+		}
+	};
+
+
 }
 
 #endif
