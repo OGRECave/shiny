666c1414a13f6515efb91019f84fef083517781c
scrawl <scrawl@baseoftrash.de>
2013-02-04 16:53:51 +0100

    removed WRITE_SHADER_DEBUG option

--- Main/ShaderInstance.cpp
+++ Main/ShaderInstance.cpp
@@ -363,12 +363,6 @@ namespace sh
 
 			if (Factory::getInstance ().getShaderDebugOutputEnabled ())
 				writeDebugFile(source, name + ".pre");
-			else
-			{
-	#ifdef SHINY_WRITE_SHADER_DEBUG
-				writeDebugFile(source, name + ".pre");
-	#endif
-			}
 
 			// why do we need our own preprocessor? there are several custom commands available in the shader files
 			// (for example for binding uniforms to properties or auto constants) - more below. it is important that these
@@ -648,12 +642,6 @@ namespace sh
 
 		if (Factory::getInstance ().getShaderDebugOutputEnabled ())
 			writeDebugFile(source, name);
-		else
-		{
-#ifdef SHINY_WRITE_SHADER_DEBUG
-			writeDebugFile(source, name);
-#endif
-		}
 
 		if (!mProgram->getSupported())
 		{
